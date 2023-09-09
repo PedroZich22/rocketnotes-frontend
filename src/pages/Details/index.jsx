5ac5f52843ff9e15daf048cc6b156dbfb7faee33
Pedro Zich <zichpedro22@gmail.com>
2023-09-09 17:18:02 -0300

    initial configuration

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+import { Button } from "../../components/Button";
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Details</h1>
+
+      <Button title="Entrar" loading/>
+    </Container>
+  );
+}
