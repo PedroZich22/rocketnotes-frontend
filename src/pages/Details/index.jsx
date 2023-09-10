94913bba538cf3c93e419786c384f334e9a99220
Pedro Zich <zichpedro22@gmail.com>
2023-09-09 21:09:03 -0300

    feat: home page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,44 @@
-import { Container } from "./styles";
+import { Container, Content, Links } from "./styles";
 import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
-      <h1>Details</h1>
+      <Header />
 
-      <Button title="Entrar" loading/>
+      <main>
+        <Content>
+          <ButtonText title="Editar" />
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+            dignissimos sint assumenda harum atque quidem voluptatibus
+            temporibus autem ab id enim repudiandae veniam molestiae, eligendi
+            expedita vero earum mollitia error.
+          </p>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
